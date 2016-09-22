27038d246857b77310fe62d2d5ba0a7ecbe1150d
Ravinder <rsingh37@sapient.com>
2016-09-22 12:36:49 +0100

    changes applied

---
+++ test-web-automation/src/test/java/com/acceptance/Test/MultiItem_AddToBagTest.java
@@ -0,0 +1,13 @@
+package com.acceptance.Test;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/feature/MultiItem_Add_to_bag.feature", glue = {"com.aceptance.stepDefs", "com.base.component"},format = {"pretty",
+        "html:target/cucumber-reports", "json:target/cucumber-reports/cucumber.json"}, tags = {"@completed"})
+public class MultiItem_AddToBagTest {
+
+}
